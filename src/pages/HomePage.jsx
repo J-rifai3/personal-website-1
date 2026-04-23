aa4e292a44b141fa9e602308fee3a2c01ac529bd
J-rifai3 <jrifai3@gatech.edu>
2026-04-23 10:34:16 -0400

    added download resume feature

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,9 @@ function HomePage() {
           Use this space to introduce yourself and your craft. Highlight what you do
           best and the type of work you are most excited to collaborate on.
         </p>
+        <a className="resume-download" href="/resume.pdf" download>
+          Download Resume
+        </a>
       </div>
       <div className="image-placeholder hero-image">Hero Image Placeholder</div>
     </section>
